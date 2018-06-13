da9f6ab0485825c9c71e98b55e3eef7e2ff4370b
William Connelly <williamconnelly.dev@gmail.com>
2018-06-12 18:49:04 -0700

    Added new pages, routeing, and styling

--- index.js
+++ index.js
@@ -9,20 +9,37 @@ app.use(bp.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "static")));
 
 app.get("/", function (req, res) {
-	// TODO: Add links to index.html so users can navigate
+	// Add links to index.html so users can navigate
 	res.sendFile(__dirname + "/static/html/index.html");
 });
 
 app.get("/skills", function (req, res) {
 	var skills = fs.readFileSync("./data.json");
 	skills = JSON.parse(skills);
-	// TODO: Render this array of skills into an EJS template
-	res.send(skills);
+	//Render this array of skills into an EJS template
+	res.render("skills", {skills: skills});
 });
 
 // TODO: Add GET route that retuns static page containing a form
+app.get("/form", function (req, res) {
+	res.sendFile(__dirname + "/static/html/form.html");
+});
 
-// TODO: Add POST route that writes new skill to the file, redirects to skills index
-// TODO Form should have action = "/skills" and method="POST"
+// Add POST route that writes new skill to the file, redirects to skills index
+// Form should have action = "/skills" and method="POST"
+
+app.post("/skills", function (req, res) {
+	// Read the data file
+	var skills = fs.readFileSync("./data.json");
+	// Turn it into an object (Parse)
+	skills = JSON.parse(skills);
+	// Add the new skill to the array (push)
+	skills.push({name: req.body.name, level: req.body.level});
+	console.log(skills);
+	// Write the object back to the file
+	fs.writeFileSync("./data.json", JSON.stringify(skills));
+	// redirect to skill list page
+	res.redirect("/skills");
+});
 
 app.listen(process.env.PORT || 3000);
